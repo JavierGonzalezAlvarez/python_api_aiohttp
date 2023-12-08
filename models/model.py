ace92a7aa824209830293e4d3a823dc86d7f865e
javier <jgalvarez.jga@gmail.com>
2023-12-08 13:11:57 +0100

    models beanie - pydantinc

---
+++ models/model.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from typing import Dict, List, Optional
+from beanie import Document, init_beanie
+from pydantic import BaseModel, Field
+
+class Address(BaseModel):
+    """
+    defines address collection
+    """
+    address: str | None = None
+    number: str | None = None
+    postal_code: str | None = None
+    province: str | None = None
+    state: str | None = None
+    status: str | None = None  # if addresses active
+    option: int | None = None  # ranking to call
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+
+class Employees(BaseModel):
+    """
+    defines employees collection
+    """
+    code_employee: str | None
+    name: Dict[str, str]  # name, first, last
+    category: List[str] = ["gardener"]
+    status: str | None
+    address_employee: List[Address] = []
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+
+class GeneralData(Document):
+    """
+    define general data
+    """
+    code: str | None
+    status: str = "deactivated"
+    company_name: str
+    juridica: Optional[List[str]] = None
+    phone1: Optional[str] = None
+    phone2: Optional[str] = None
+    address: List[Address] | None = []
+    employees: List[Employees] | None = []
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+    class Settings:
+        """
+        name by default of the collection
+        """
+
+        name = "general_data"
+
